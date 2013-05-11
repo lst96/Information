8d15a9fa183801f1847bcdbc147064484cba9074
lst96 <trexler96@gmail.com>
2013-05-11 14:53:34 -0400

    Change agh to something else

--- src/io/github/lst96/Information/Information.java
+++ src/io/github/lst96/Information/Information.java
@@ -22,7 +22,7 @@ public class Information extends JavaPlugin {
 	   getCommand("inforeload").setExecutor(new Commands(this)); 
 	   getCommand("staff").setExecutor(new Commands(this)); 
 	   getCommand("rules").setExecutor(new Commands(this));
-	   getCommand("Agh").setExecutor(new Commands(this));
+	   getCommand("got push").setExecutor(new Commands(this));
    }
    public void onDisable(){
 	   PluginDescriptionFile pdfFile = getDescription();
