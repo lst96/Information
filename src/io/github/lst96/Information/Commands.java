c06801f4a0ff3beca98cf1f03f8a99fd9a682108
lst96 <trexler96@gmail.com>
2013-05-11 13:09:46 -0400

    Added Commands Split java class files up Added Color

---
+++ src/io/github/lst96/Information/Commands.java
@@ -0,0 +1,64 @@
+package io.github.lst96.Information;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+
+public class Commands implements CommandExecutor{
+	
+	private Information plugin; // pointer to your main class, unrequired if you don't need methods from the main class
+	 
+    public Commands(Information instance) {
+        this.plugin = instance;
+    }
+	
+public boolean  onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+	   {
+		   Player player = (Player) sender;
+   
+  if(commandLabel.equalsIgnoreCase("website")){
+	   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.DARK_GREEN + plugin.getConfig().getString("Website"));
+  }
+
+  if(commandLabel.equalsIgnoreCase("donate")){
+	   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.DARK_BLUE + plugin.getConfig().getString("Donate"));
+  }
+
+  if(commandLabel.equalsIgnoreCase("vote")){
+	   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.DARK_AQUA + plugin.getConfig().getString("VoteLink1"));
+  }
+
+  if(commandLabel.equalsIgnoreCase("vote")){
+	   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.DARK_PURPLE + plugin.getConfig().getString("VoteLink2"));
+  }
+
+  if(commandLabel.equalsIgnoreCase("vote")){
+	   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.GOLD + plugin.getConfig().getString("VoteLink3"));
+  }
+
+  if(commandLabel.equalsIgnoreCase("vote")){
+	   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.GREEN + plugin.getConfig().getString("VoteLink4"));
+  }
+  
+  if(commandLabel.equalsIgnoreCase("staff")){
+	   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.YELLOW + plugin.getConfig().getString("Staff"));
+  }
+
+  if(commandLabel.equalsIgnoreCase("rules")){
+	   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.AQUA + plugin.getConfig().getString("Rules"));
+  }
+
+  if(commandLabel.equalsIgnoreCase("inforeload")){
+	   plugin.reloadConfig();
+	   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.RED + " Configuration Reloaded!");
+	   
+  return true;
+	   }
+  return false;
+    }
+}
+	   
+
+	   
---
+++ src/io/github/lst96/Information/Information.java
@@ -0,0 +1,30 @@
+package io.github.lst96.Information;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Information extends JavaPlugin {
+	
+	public final Logger logger = Logger.getLogger("Minecraft");
+	public static Information plugin;
+	
+   @Override 
+   public void onEnable (){
+	   PluginDescriptionFile pdfFile = getDescription();
+	   this.logger.info(pdfFile.getName() + " v" + pdfFile.getVersion() + " is enabled.");
+	   getConfig().options().copyDefaults(true);
+	   saveConfig();
+	   getCommand("website").setExecutor(new Commands(this)); 
+	   getCommand("donate").setExecutor(new Commands(this)); 
+	   getCommand("vote").setExecutor(new Commands(this)); 
+	   getCommand("inforeload").setExecutor(new Commands(this)); 
+	   getCommand("staff").setExecutor(new Commands(this)); 
+	   getCommand("rules").setExecutor(new Commands(this));
+   }
+   public void onDisable(){
+	   PluginDescriptionFile pdfFile = getDescription();
+	   this.logger.info(pdfFile.getName() + " v" + pdfFile.getVersion() + " is Disabled.");
+   }
+}
