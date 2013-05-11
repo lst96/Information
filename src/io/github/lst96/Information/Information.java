3056655e489bb808e7f19570d167431a3f2010b4
lst96 <trexler96@gmail.com>
2013-05-11 14:27:03 -0400

    changed testing to CoolCraft

--- src/io/github/lst96/Information/Information.java
+++ src/io/github/lst96/Information/Information.java
@@ -22,7 +22,7 @@ public class Information extends JavaPlugin {
 	   getCommand("inforeload").setExecutor(new Commands(this)); 
 	   getCommand("staff").setExecutor(new Commands(this)); 
 	   getCommand("rules").setExecutor(new Commands(this));
-	   getCommand("Testing").setExecutor(new Commands(this));
+	   getCommand("CoolCraft").setExecutor(new Commands(this));
    }
    public void onDisable(){
 	   PluginDescriptionFile pdfFile = getDescription();
