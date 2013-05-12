b4536367fad4c40a4eea4e4cfabea37325f6771c
Hodgetts330 <hodgetts330@gmail.com>
2013-05-12 01:13:32 +0100

    Added Stuff To Tps.java

--- src/io/github/lst96/Information/Tps.java
+++ src/io/github/lst96/Information/Tps.java
@@ -4,8 +4,45 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.ChatColor;
 
 public class Tps {
+	
+	private int tps = 0;
+
+	public void onEnable()
+	{
+	getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable()
+	{
+	    long sec;
+	    long currentSec;
+	    int  ticks;
+	    int  delay;
+	   
+	    @Override
+	    public void run()
+	    {
+	        sec = (System.currentTimeMillis() / 1000);
+	       
+	        if(currentSec == sec)
+	        {
+	            ticks++;
+	        }
+	        else
+	        {
+	            currentSec = sec;
+	            tps = (tps == 0 ? ticks : ((tps + ticks) / 2));
+	            ticks = 0;
+	        }
+	    }
+	}, 0, 1);
+	}
+
+	@EventHandler
+	public void eventBlockPlace(BlockPlaceEvent event)
+	{
+	    event.getPlayer().sendMessage("TPS = " + tps);
+	}
 
 }
