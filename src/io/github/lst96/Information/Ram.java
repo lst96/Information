bd719336ff65d1cb4590c50dfb5065abd33202ba
lst96 <trexler96@gmail.com>
2013-05-12 00:25:50 -0400

    Added uptime to /ram

---
+++ src/io/github/lst96/Information/Ram.java
@@ -0,0 +1,35 @@
+package io.github.lst96.Information;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Ram implements CommandExecutor{
+	
+	private long serverStart = System.currentTimeMillis();
+	
+	private Information plugin; // pointer to your main class, unrequired if you don't need methods from the main class
+	 
+    public Ram(Information instance) {
+        this.plugin = instance;
+        
+    }
+        
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            Runtime runtime = Runtime.getRuntime();
+            System.gc();
+            if ((sender.isOp()) || (sender.hasPermission("information.ram"))) {
+              long diff = System.currentTimeMillis() - this.serverStart;
+              sender.sendMessage(ChatColor.GOLD + "[Uptime]: " + (int)(diff / 86400000L) + "d" + " " + (int)(diff / 3600000L % 24L) + "h" + " " + (int)(diff / 60000L % 60L) + "m" + " " + (int)(diff / 1000L % 60L) + "s");
+              sender.sendMessage(ChatColor.GOLD + "[Maximum Ram]: " + ChatColor.RESET + ChatColor.RED + runtime.maxMemory() / 1048576L + " MB");
+              sender.sendMessage(ChatColor.GOLD + "[Allocated Ram]: " + ChatColor.RESET + ChatColor.RED + runtime.totalMemory() / 1048576L + " MB");
+              sender.sendMessage(ChatColor.GOLD + "[Used Ram]: " + ChatColor.RESET + ChatColor.RED + (runtime.totalMemory() - runtime.freeMemory()) / 1048576L + " MB");
+              sender.sendMessage(ChatColor.GOLD + "[Free Ram]: " + ChatColor.RESET + ChatColor.RED + runtime.freeMemory() / 1048576L + " MB");
+              return true;
+            }
+
+            sender.sendMessage(ChatColor.DARK_RED + "[Information]" + ChatColor.RED + "You don't have permissions.");
+            return true;
+        }
+}
