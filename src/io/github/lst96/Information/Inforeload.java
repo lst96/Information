2ec83f4548d84ce539e09a555be6aea24b8c4ce8
lst96 <trexler96@gmail.com>
2013-05-12 16:08:27 -0400

    Fixed permission for inforeload.

--- src/io/github/lst96/Information/Inforeload.java
+++ src/io/github/lst96/Information/Inforeload.java
@@ -20,11 +20,13 @@ public class Inforeload implements CommandExecutor{
 
     	if (commandLabel.equalsIgnoreCase("inforeload")) {
     		  if(sender instanceof Player){
+    			  if (sender.hasPermission("information.reload"));
     	      plugin.reloadConfig();
     	          sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.RED + " Configuration Reloaded!");
     		  }else{
     			  plugin.reloadConfig();
     			  sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.RED + " Configuration Reloaded!");
+    			  sender.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error."); 
     		  }
       return true;
     }
