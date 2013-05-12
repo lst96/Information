4e6e11c2d6e1e6be1b2738596fa245e97b437d70
lst96 <trexler96@gmail.com>
2013-05-12 17:15:48 -0400

    Fixed /inforeload permission.

--- src/io/github/lst96/Information/Inforeload.java
+++ src/io/github/lst96/Information/Inforeload.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 public class Inforeload implements CommandExecutor{
@@ -17,19 +18,16 @@ public class Inforeload implements CommandExecutor{
     
     public boolean  onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
     {
-
-    	if (commandLabel.equalsIgnoreCase("inforeload")) {
-    		  if(sender instanceof Player){
-    			  if (sender.hasPermission("information.reload"));
-    	      plugin.reloadConfig();
-    	          sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.RED + " Configuration Reloaded!");
-    		  }else{
-    			  plugin.reloadConfig();
-    			  sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.RED + " Configuration Reloaded!");
-    			  sender.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error."); 
-    		  }
-      return true;
+    	if(commandLabel.equalsIgnoreCase("inforeload")){
+    		if ((sender.isOp()) || (sender.hasPermission("information.ram"))) {
+    			plugin.reloadConfig();	
+    			sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.RED + " Configuration Reloaded!");
+    		return true;
+    		}
+    		
+    		sender.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
+    		return true;
+    	}
+    	return false;
     }
-    return false;
-    	   }
-    }
+}
