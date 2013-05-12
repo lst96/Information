560a44a4a100626057d018d9f435e6b7c379f248
lst96 <trexler96@gmail.com>
2013-05-12 17:20:34 -0400

    Fixed online.java permission.

--- src/io/github/lst96/Information/Online.java
+++ src/io/github/lst96/Information/Online.java
@@ -21,13 +21,12 @@ public class Online implements CommandExecutor{
     {
     	
     	if(commandLabel.equalsIgnoreCase("online")){
-    		Player player = (Player) sender;
-    	    if(player.hasPermission("information.online")) {
-    		   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.YELLOW + Bukkit.getOnlinePlayers().length + " " + "of" + " " + ChatColor.GREEN + Bukkit.getMaxPlayers());
-    	    }else{
-    		   player.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
+    		if ((sender.isOp()) || (sender.hasPermission("information.online"))) {
+    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.YELLOW + Bukkit.getOnlinePlayers().length + " " + "of" + " " + ChatColor.GREEN + Bukkit.getMaxPlayers());
+    	    return true;
     		}
-    	return true;
+    		sender.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
+    		return true;
     	}
     	return false;
     }
