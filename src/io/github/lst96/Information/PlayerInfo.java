48c50713c20e2df09702185b17db8fabe9bb7cb8
lst96 <trexler96@gmail.com>
2013-05-13 17:02:06 -0400

    Updated PlayerInfo.java

--- src/io/github/lst96/Information/PlayerInfo.java
+++ src/io/github/lst96/Information/PlayerInfo.java
@@ -29,7 +29,6 @@ public class PlayerInfo
         return true;
       }
       int health = targetplayer.getPlayer().getHealth();
-      long firstPlayed = targetplayer.getPlayer().getFirstPlayed();
       int foodLevel = targetplayer.getPlayer().getFoodLevel();
       float exp = targetplayer.getPlayer().getLevel();
       InetSocketAddress playerip = targetplayer.getAddress();
@@ -37,13 +36,11 @@ public class PlayerInfo
       String playername = targetplayer.getName();
       boolean flyCheck = targetplayer.isFlying();
       boolean opCheck = targetplayer.isOp();
-      long timeonserver = Bukkit.getServer().getPlayer(args[0]).getPlayer().getPlayerTime();
       Location location = targetplayer.getLocation();
       int fireCheck = targetplayer.getFireTicks();
       sender.sendMessage(ChatColor.GOLD + playername + " health is at: " + health + "/20");
       sender.sendMessage(ChatColor.GOLD + playername + "'s Location: " + "X: " + location.getBlockX() + ", Y: " + location.getBlockY() + ", Z: " + location.getBlockZ());
       sender.sendMessage(ChatColor.GOLD + playername + " is in world: " + location.getWorld().getName());
-      sender.sendMessage(ChatColor.GOLD + playername + " First Played: " + firstPlayed);
       sender.sendMessage(ChatColor.GOLD + playername + "'s food level is at: " + foodLevel + "/20");
       sender.sendMessage(ChatColor.GOLD + playername + " Gamemode is: " + gamemode);
       sender.sendMessage(ChatColor.GOLD + playername + " exp level is " + exp);
@@ -51,7 +48,6 @@ public class PlayerInfo
       sender.sendMessage(ChatColor.GOLD + playername + " is flying: " + flyCheck);
       sender.sendMessage(ChatColor.GOLD + playername + " is op: " + opCheck);
       sender.sendMessage(ChatColor.GOLD + playername + " is on fire: " + fireCheck);
-      sender.sendMessage(ChatColor.GOLD + playername + " Time On Server: " + timeonserver);
     }
 
     return false;
