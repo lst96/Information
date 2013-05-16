2f90fabb350509ee1041bc0c6b451672c2c539ac
lst96 <trexler96@gmail.com>
2013-05-16 18:23:18 -0400

    Recoded vote.java (console support)

--- src/io/github/lst96/Information/Vote.java
+++ src/io/github/lst96/Information/Vote.java
@@ -19,9 +19,9 @@ public class Vote implements CommandExecutor{
     	if(commandLabel.equalsIgnoreCase("vote")){
     		if ((sender.isOp()) || (sender.hasPermission("information.vote"))) {
     		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.AQUA + plugin.getConfig().getString("VoteLink1"));
-    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.AQUA + plugin.getConfig().getString("VoteLink2"));
-    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.AQUA + plugin.getConfig().getString("VoteLink3"));
-    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.AQUA + plugin.getConfig().getString("VoteLink4"));
+    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.DARK_PURPLE + plugin.getConfig().getString("VoteLink2"));
+    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.GOLD + plugin.getConfig().getString("VoteLink3"));
+    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.GREEN + plugin.getConfig().getString("VoteLink4"));
     		   return true;
     		}
     		sender.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
