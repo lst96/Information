2dcc47adce5b5c7a591c905e84be332f893001c6
lst96 <trexler96@gmail.com>
2013-05-16 18:22:25 -0400

    Recoded vote.java (console support)

--- src/io/github/lst96/Information/Vote.java
+++ src/io/github/lst96/Information/Vote.java
@@ -3,7 +3,6 @@ package io.github.lst96.Information;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 
 public class Vote implements CommandExecutor{
@@ -17,43 +16,19 @@ public class Vote implements CommandExecutor{
     
     public boolean  onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
     {
-
     	if(commandLabel.equalsIgnoreCase("vote")){
-    		Player player = (Player) sender;
-    	if(player.hasPermission("information.vote")) {
-    		   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.DARK_AQUA + plugin.getConfig().getString("VoteLink1"));
-    		}else{
-    		   player.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
+    		if ((sender.isOp()) || (sender.hasPermission("information.vote"))) {
+    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.AQUA + plugin.getConfig().getString("VoteLink1"));
+    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.AQUA + plugin.getConfig().getString("VoteLink2"));
+    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.AQUA + plugin.getConfig().getString("VoteLink3"));
+    		   sender.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.AQUA + plugin.getConfig().getString("VoteLink4"));
+    		   return true;
     		}
-    	}
-
-    	if(commandLabel.equalsIgnoreCase("vote")){
-    		Player player = (Player) sender;
-    	if(player.hasPermission("information.vote")) {
-    		   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.DARK_PURPLE + plugin.getConfig().getString("VoteLink2"));
-    		}else{
-    		   player.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
-    		}
-    	}
-
-    	if(commandLabel.equalsIgnoreCase("vote")){
-    		Player player = (Player) sender;
-    	if(player.hasPermission("information.vote")) {
-    		   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.GOLD + plugin.getConfig().getString("VoteLink3"));
-    		}else{
-    		   player.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
-    		}
-    	}
-    	
-    	if(commandLabel.equalsIgnoreCase("vote")){
-    		Player player = (Player) sender;
-    	if(player.hasPermission("information.vote")) {
-    		   player.sendMessage(ChatColor.DARK_RED + "[Information]" + " " + ChatColor.GREEN + plugin.getConfig().getString("VoteLink4"));
-    		}else{
-    		   player.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
-    		}
-    	return true;
+    		sender.sendMessage(ChatColor.DARK_RED + "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
+    		return true;
     	}
     	return false;
+      }
     }
-}
+    		   
+    	    
